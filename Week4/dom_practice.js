af052bf96f3c176453190f52621eba1e31203642
LichtLights <rlatjdwns151@outlook.com>
2023-03-29 15:43:13 +0900

    practice commit

--- Week4/dom_practice.js
+++ Week4/dom_practice.js
@@ -1,36 +1,13 @@
-// DOM 조작하기
-let statusNum = 1;
-const myParagraph = document.getElementById("my-paragraph");
-
-myParagraph.innerHTML = "This is a new paragraph.";
-myParagraph.style.color = "red";
-
-// 이벤트 처리 방법
-const addButton = document.getElementById("add-button");
-
-addButton.addEventListener("click", function () {
-  const actionStatus = document.getElementById("action-status");
-
-  let clickStatus = document.createElement("div");
-  clickStatus.setAttribute("class", "status");
-  clickStatus.innerHTML = String(statusNum) + ": Button clicked!";
-  statusNum += 1;
-
-  actionStatus.appendChild(clickStatus);
-});
-
-const delButton = document.getElementById("del-button");
-
-delButton.addEventListener("click", function () {
-  const status = document.getElementsByClassName("status");
-  status[status.length - 1].remove();
-  statusNum -= 1;
-});
-
-const clrButton = document.getElementById("clr-button");
-
-clrButton.addEventListener("click", function () {
-  const status = document.getElementsByClassName("status");
-  status.splice(0, status.length);
-  statusNum = 0;
-});
+function calculator(value) {
+  if(value == "C"){
+    document.getElementById('result').value = "";
+  }
+  else{
+    document.getElementById('result').value += value;
+  }
+  
+}
+function calculate() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = eval(result);
+}
